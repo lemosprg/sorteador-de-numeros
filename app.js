73d149968088c3901bc06a8711241ea7dbb123db
Pablo Lemos <lemosprg@gmail.com>
2024-02-19 12:12:25 -0300

    Conclusão - Ajuste de Botões e Respostas

--- app.js
+++ app.js
@@ -23,9 +23,45 @@ function sortear() {
     //Deixar a lista de sorteados em ordem crescente de amostragem
     numerosSorteados.sort((a, b) => a-b);
     //Teste de funcionalidade dos numeros sorteados
-    alert(numerosSorteados);
+    //alert(numerosSorteados);
+
+    let resultados = document.getElementById('resultado');
+    resultados.innerHTML = `<label class="texto__paragrafo__fim">Números Sorteados: ${numerosSorteados}.</label>`;
+
+    alterarBotoes();
+}
+
+function reiniciar(){
+    let resultadosReiniar = document.getElementById('resultado');
+    resultadosReiniar.innerHTML = `<label class="texto__paragrafo__fim">Preencha os campos acima e clique em "Sortear"</label>`;
+
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    
+    alterarBotoes();
 }
 
 function gerandoNumeroAleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+//Função para modificação dos botões de Sorteio e Reinicio de jogo
+function alterarBotoes(){
+    let botaoReiniciar = document.getElementById('btn-reiniciar');
+    let botaoSortear = document.getElementById('btn-sortear');
+
+    if (botaoReiniciar.classList.contains('container__botao-desabilitado')){
+        botaoReiniciar.classList.remove('container__botao-desabilitado');
+        botaoReiniciar.classList.add('container__botao');
+    
+        botaoSortear.classList.remove('container__botao');
+        botaoSortear.classList.add('container__botao-desabilitado');
+    } else{
+        botaoReiniciar.classList.remove('container__botao');
+        botaoReiniciar.classList.add('container__botao-desabilitado');
+    
+        botaoSortear.classList.remove('container__botao-desabilitado');
+        botaoSortear.classList.add('container__botao');
+    }
 }
